17e16be6f943979d7361feba0a86075b30eb1395
Faria-123 <mrsnoushin1987@gmail.com>
2026-05-13 22:44:25 +0600

    here img

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,17 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       hostname: 'unsplash.com'
-  //     }
-  //   ]
-  // }
   images: {
-    protocol: 'https',
-    domains: ['images.unsplash.com'],
-  },
+    remotePatterns: [
+      {
+        hostname: 'images.unsplash.com'
+      }
+    ]
+  }
+  // images: {
+  //   protocol: 'https',
+  //   domains: ['images.unsplash.com'],
+  // },
 };
 
 export default nextConfig;
